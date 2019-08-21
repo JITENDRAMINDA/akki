3bfda1da4cfbb9a630e1d08d0bd66bece49e3756
JITENDRAMINDA <49564169+JITENDRAMINDA@users.noreply.github.com>
2019-08-21 13:31:21 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,9 +5,13 @@ app = Client ("ssss",bot_token="845406847:AAGN2pODnH_IOUjS7PIEfePxn4XXQcv2YFY",a
 bullet = -1001378725482                                              
 ferrari = -1001274887387                                             
 k = -1001472962317
+
+message.text.replace("🇩🇪","🇳🇮").replace("🎾","🥎").replace("🖲","🧤").replace("📟","🛑").replace("WD","🔷 WIDE BALL 🔷").replace("/","~").replace("CHALU RAKHO","GAME STARTED, PLAYERS ON THE STEDIUM ").replace("NB","🔷 NO BALL 🔷")
+  
+
 @app.on_message(Filters.chat(ferrari) & ~ Filters.edited)
 def main(client, message):
- mes = client.send_message( k, "**" + message.text + "**" )
+ mes = client.send_message( k, "**" + message.text.replace("🇩🇪","🇸🇱").replace("🎾","🥎").replace("🖲","👨‍🎨").replace("📟","🏮").replace("WD","🔰 WIDE BALL 🔰").replace("/","🔸").replace("CHALU RAKHO","🔶 GAME STARTED 🔶, PLAYERS ON THE STEDIUM ").replace("NB","♦️ NO BALL ♦️") + "**" )
  fie = open("ids.txt","a")
  fie.write(" " + str(message.message_id) + " " + str(mes.message_id))
  fie.close()
@@ -30,7 +34,7 @@ def main(client, message):
      if message.text == ".":   
       client.delete_messages(k,int(x[x.index(id)+1]))
      else:
-      client.edit_message_text(k,int(x[x.index(id)+1]), "**" + message.text + "**" )
+      client.edit_message_text(k,int(x[x.index(id)+1]), "**" + message.text.replace("🇩🇪","🇸🇱").replace("🎾","🥎").replace("🖲","👨‍🎨").replace("📟","🏮").replace("WD","🔰 WIDE BALL 🔰").replace("/","🔸").replace("CHALU RAKHO","🔶 GAME STARTED 🔶, PLAYERS ON THE STEDIUM ").replace("NB","♦️ NO BALL ♦️") + "**" )
    except FloodWait as e:
      time.sleep(e.x)
 @app.on_message(Filters.chat(ferrari) & Filters.edited)
