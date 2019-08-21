fe2ee975dbbbd09ff3be87ce91b2c3c841121679
JITENDRAMINDA <49564169+JITENDRAMINDA@users.noreply.github.com>
2019-08-21 15:11:54 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,13 +1,13 @@
 from pyrogram import Client, Filters
 import time
 from pyrogram.errors import FloodWait
-app = Client ("ssss",bot_token="890077588:AAF5O8SGsliDMSRCGQUEUchBlzAMIs0qTgo",api_id=814511,api_hash="44462f0f278503255d5cc30941b617a9")                                   
+app = Client ("ssss",bot_token="967722305:AAGZTeCIpBCSJFelnANrgRCPYjzy4UBxHlQ",api_id=814511,api_hash="44462f0f278503255d5cc30941b617a9")                                   
 bullet = -1001378725482                                              
 ferrari = -1001274887387                                             
-k = -1001365164207
+k = -1001224891050
 @app.on_message(Filters.chat(bullet) & ~ Filters.edited)
 def main(client, message):
- mes = client.send_message( k, "**" + message.text.replace("🇩🇪","🇱🇸").replace("🎾","🥎").replace("🖲","🧸").replace("📟","📮").replace("WD","🏷️ WIDE BALL 🏷️").replace("/","🔹").replace("CHALU RAKHO","💠 GAME STARTED 💠, PLAYERS ON THE STEDIUM ").replace("NB","⚕️ NO BALL ⚕️") + "**" )
+ mes = client.send_message( k, "**" + message.text.replace("🇩🇪","🇸🇱").replace("🎾","🥎").replace("🖲","🧤").replace("📟","🏮").replace("WD","🔷 WIDE BALL 🔷").replace("/","~").replace("CHALU RAKHO","🔶 GAME STARTED 🔶, PLAYERS ON THE STEDIUM ").replace("NB","♦️ NO BALL ♦️") + "**" )
  fie = open("ids.txt","a")
  fie.write(" " + str(message.message_id) + " " + str(mes.message_id))
  fie.close()
@@ -24,7 +24,7 @@ def main(client, message):
      if message.text == ".":   
       client.delete_messages(k,int(x[x.index(id)+1]))
      else:
-      client.edit_message_text(k,int(x[x.index(id)+1]), "**" + message.text.replace("🇩🇪","🇸🇱").replace("🎾","🥎").replace("🖲","👨‍🎨").replace("📟","🏮").replace("WD","🔰 WIDE BALL 🔰").replace("/","🔸").replace("CHALU RAKHO","🔶 GAME STARTED 🔶, PLAYERS ON THE STEDIUM ").replace("NB","♦️ NO BALL ♦️") + "**" )
+      client.edit_message_text(k,int(x[x.index(id)+1]), "**" + message.text.replace("🇩🇪","🇸🇱").replace("🎾","🥎").replace("🖲","🧤").replace("📟","🏮").replace("WD","🔷 WIDE BALL 🔷").replace("/","~").replace("CHALU RAKHO","🔶 GAME STARTED 🔶, PLAYERS ON THE STEDIUM ").replace("NB","♦️ NO BALL ♦️") + "**" )
    except FloodWait as e:
      time.sleep(e.x)
 @app.on_message(Filters.chat(ferrari) & ~ Filters.edited)
