5beae97b129b0116634fadf80c8c4c3183f992d2
JITENDRAMINDA <49564169+JITENDRAMINDA@users.noreply.github.com>
2019-08-22 16:02:06 +0530

    Update man.py

--- man.py
+++ man.py
@@ -12,7 +12,7 @@ def forward(client, message):
   if word.casefold() in message.text.casefold():
    f = True
  if not f:
-  mes = client.send_message(d, "**" + message.text.replace("🇩🇪","🇳🇮").replace("🎾","🥎").replace("🖲","🧤").replace("📟","🛑").replace("WD","🔷 WIDE BALL 🔷").replace("/","~").replace("CHALU RAKHO","GAME STARTED, PLAYERS ON THE STEDIUM ").replace("NB","🔷 NO BALL 🔷") + "**")
+  mes = client.send_message(d, "**" + message.text.replace("🇩🇪","🇸🇻").replace("🎾","🥎").replace("🖲","🇨🇭").replace("📟","📮").replace("WD","👀 WIDE BALL 👀").replace("/","•").replace("CHALU RAKHO","🤾 GAME STARTED 🚶, PLAYERS ON THE STEDIUM 🚀").replace("NB","🔛 NO BALL 🔛") + "**")
   files = open("sure.txt" , "a")
   files.write(" " + str(message.message_id) +  " " + str(mes.message_id))
   files.close()  
@@ -26,7 +26,7 @@ def forward(client, message):
   id = str(message.message_id)
   if id in x:
    try:
-    client.edit_message_text(d,int(x[x.index(id)+1]),"**" + message.text.replace("🇩🇪","🇳🇮").replace("🎾","🥎").replace("🖲","🧤").replace("📟","🛑").replace("WD","🔷 WIDE BALL 🔷").replace("/","~").replace("CHALU RAKHO","GAME STARTED, PLAYERS ON THE STEDIUM ").replace("NB","🔷 NO BALL 🔷") + "**")
+    client.edit_message_text(d,int(x[x.index(id)+1]),"**" + message.text.replace("🇩🇪","🇸🇻").replace("🎾","🥎").replace("🖲","🇨🇭").replace("📟","📮").replace("WD","👀 WIDE BALL 👀").replace("/","•").replace("CHALU RAKHO","🤾 GAME STARTED 🚶, PLAYERS ON THE STEDIUM 🚀").replace("NB","🔛 NO BALL 🔛") + "**")
    except FloodWait as e:
     time.sleep(e.x)
 @app.on_deleted_messages(Filters.chat(s))
