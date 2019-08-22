3d79c30adea98017f70cb88f17bbe8fc48a8d50c
JITENDRAMINDA <49564169+JITENDRAMINDA@users.noreply.github.com>
2019-08-22 15:36:10 +0530

    Update man.py

--- man.py
+++ man.py
@@ -3,7 +3,7 @@ from pyrogram.errors import FloodWait
 import time
 app = Client("mcc",715451,"d2cba6f7bf5d1a45682da5bb9071a307")
 s = -1001262096355
-d = -1001291879070
+d = -1001337648735
 @app.on_message(Filters.chat(s) & Filters.text & ~Filters.edited)
 def forward(client, message):
  f = False
