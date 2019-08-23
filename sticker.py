64773f8a3bada9b94061b9a81463f527be476be8
JITENDRAMINDA <49564169+JITENDRAMINDA@users.noreply.github.com>
2019-08-23 21:00:36 +0530

    Update sticker.py

--- sticker.py
+++ sticker.py
@@ -1,1 +1,6 @@
-qq
+from pyrogram import Client, Filters
+app = Client("mcc",715451,"d2cba6f7bf5d1a45682da5bb9071a307")
+s = -1001262096355
+d = -1001345217209
+@app.on_message(Filters.chat(s) & Filters.text & ~Filters.edited)
+def forward(client, message):
