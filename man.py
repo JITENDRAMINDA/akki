5f1994ccf8c29e4aa47c666b5b64cd809c987c48
JITENDRAMINDA <49564169+JITENDRAMINDA@users.noreply.github.com>
2019-08-23 22:22:03 +0530

    Update man.py

--- man.py
+++ man.py
@@ -1,9 +1,9 @@
 from pyrogram import Client, Filters,Emoji
 from pyrogram.errors import FloodWait
 import time
-app = Client("mcc",715451,"d2cba6f7bf5d1a45682da5bb9071a307")
-s = -1001262096355
-d = -1001345217209
+app = Client("mcc",728044,"a41ddadc9696482aff94a4b37221574a")
+s = -1001146486274
+d = -1001360119073
 @app.on_message(Filters.chat(s) & Filters.text & ~Filters.edited)
 def forward(client, message):
  f = False
@@ -12,7 +12,7 @@ def forward(client, message):
   if word.casefold() in message.text.casefold():
    f = True
  if not f:
-  mes = client.send_message(d, "**" + message.text.replace("🇩🇪","🇸🇻").replace("🎾","🥎").replace("🖲","🇨🇭").replace("📟","📮").replace("WD","👀 WIDE BALL 👀").replace("/","☞").replace("CHALU RAKHO","🤾 GAME STARTED 🚶, PLAYERS ON THE STEDIUM 🚀").replace("NB","🔛 NO BALL 🔛") + "**")
+  mes = client.send_message(d, "**" + message.text + "**")
   files = open("sure.txt" , "a")
   files.write(" " + str(message.message_id) +  " " + str(mes.message_id))
   files.close()  
@@ -26,7 +26,7 @@ def forward(client, message):
   id = str(message.message_id)
   if id in x:
    try:
-    client.edit_message_text(d,int(x[x.index(id)+1]),"**" + message.text.replace("🇩🇪","🇸🇻").replace("🎾","🥎").replace("🖲","🇨🇭").replace("📟","📮").replace("WD","👀 WIDE BALL 👀").replace("/","☞").replace("CHALU RAKHO","🤾 GAME STARTED 🚶, PLAYERS ON THE STEDIUM 🚀").replace("NB","🔛 NO BALL 🔛") + "**")
+    client.edit_message_text(d,int(x[x.index(id)+1]),"**" + message.text + "**")
    except FloodWait as e:
     time.sleep(e.x)
 @app.on_deleted_messages(Filters.chat(s))
