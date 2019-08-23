de995f4a38089e79d42f1f2d175c50e944b69db8
JITENDRAMINDA <49564169+JITENDRAMINDA@users.noreply.github.com>
2019-08-23 21:02:19 +0530

    Update sticker.py

--- sticker.py
+++ sticker.py
@@ -1,6 +1,9 @@
 from pyrogram import Client, Filters
-app = Client("mcc",715451,"d2cba6f7bf5d1a45682da5bb9071a307")
-s = -1001262096355
-d = -1001345217209
-@app.on_message(Filters.chat(s) & Filters.text & ~Filters.edited)
-def forward(client, message):
+import time
+from pyrogram.errors import FloodWait
+app = Client ("ssss",bot_token="790494538:AAHEtFqb6yxTGIjjcQ3O4JCYOtlhS1hU8oQ",api_id=814511,api_hash="44462f0f278503255d5cc30941b617a9")                                   
+
+@app.on_message(Filters.channel)
+def main(client, message):
+ if message.text == "6":
+  c
