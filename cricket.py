01e2277a786c073435eefaeedc0ff4b221e09d82
JITENDRAMINDA <49564169+JITENDRAMINDA@users.noreply.github.com>
2019-09-03 19:55:49 +0530

    Create cricket.py

---
+++ cricket.py
@@ -0,0 +1,6 @@
+from pyrogram import Client, Filters,Emoji
+from pyrogram.errors import FloodWait
+import time
+app = Client ("mcccc",728044,"a41ddadc9696482aff94a4b37221574a")
+s = -1001062910865
+d = -1001360119073
