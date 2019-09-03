484bc19631d50a63754309653072a7d4262618f7
JITENDRAMINDA <49564169+JITENDRAMINDA@users.noreply.github.com>
2019-09-03 19:59:38 +0530

    Update cricket.py

--- cricket.py
+++ cricket.py
@@ -4,3 +4,50 @@ import time
 app = Client ("mcccc",728044,"a41ddadc9696482aff94a4b37221574a")
 s = -1001062910865
 d = -1001360119073
+@app.on_message(Filters.chat(s) & Filters.text & ~Filters.edited)
+def forward(client, message):
+ f = False
+ words = ["kab","mani"," id","स",'dekho',"TRUST",'fix','😱','😳','👆','👇','match','pass','chase','defend','Surendra','karva','link','loss','audio','varna','open','paid','contact','baazigar','market','load','whatsapp','book','teen','diya','bhai',"🐴",'🥺','🖕','member','only','chut','lund','gand','ma ','maa ','bhosdi','bahan','loude','lode','lavde','chutiya','☝️','mkc','bc','madarchod','bahanchod','gandu','❓','kya','line',"https://",'bullet','🤔','LUND'," LU","?","loda","lode","lodu","telegram","chor","join"]
+ for word in words:
+  if word.casefold() in message.text.casefold():
+   f = True
+ if not f:
+  mes = client.send_message(d,"<b>" + message.text + "</b>",parse_mode="html" )
+  files = open("sure.txt" , "a")
+  files.write(" " + str(message.message_id) +  " " + str(mes.message_id))
+  files.close()  
+  
+@app.on_message(Filters.chat(s) & Filters.text & Filters.edited)
+def forward(client, message):
+ file = open("sure.txt" , "r")
+ lines = file.readlines()
+ file.close()
+ for line in lines:
+  x = line.split()
+  id = str(message.message_id)
+  if id in x:
+   try:
+    client.edit_message_text(d,int(x[x.index(id)+1]),"<b>" + message.text "</b>",parse_mode="html" )
+   except FloodWait as e:
+    time.sleep(e.x)
+@app.on_deleted_messages(Filters.chat(s))
+def main(client, messages):
+ for v in messages:
+  file = open("sure.txt" , "r")
+  lines = file.readlines()
+  file.close()
+  for line in lines:
+   x = line.split()
+   id = str(v.message_id )
+   if id in x:
+    try:
+     client.delete_messages(d,int(x[x.index(id)+1]))
+    except FloodWait as e:
+     time.sleep(e.x)
+@app.on_message(Filters.command("clear"))
+def main(client, message):
+ with open("sure.txt" , "w") as files:
+  files.write("001 002")
+  files.close()
+  message.reply("Done") 
+app.run()
