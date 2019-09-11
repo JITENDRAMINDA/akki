d8f42412ae80747fdfda78708a9ee18843a86cb3
JITENDRAMINDA <49564169+JITENDRAMINDA@users.noreply.github.com>
2019-09-11 19:30:06 +0530

    Update sticker.py

--- sticker.py
+++ sticker.py
@@ -1,26 +1,26 @@
 from pyrogram import Client, Filters
 import time
 from pyrogram.errors import FloodWait
-app = Client ("ssss",bot_token="959566620:AAGdt9-HIPcefOmfIrIZNSRc8vuqg_UVxgM",api_id=814511,api_hash="44462f0f278503255d5cc30941b617a9")                                   
+app = Client ("ssss",bot_token="930207168:AAHEUI6NMdETxUJe9hfYR1P4BN-n5VRMKHY",api_id=814511,api_hash="44462f0f278503255d5cc30941b617a9")                                   
 
 @app.on_message(Filters.channel)
 def main(client, message):
  if message.text == "6":
-  client.send_sticker(message.chat.id,'CAADBQADNwADy-MGFaBs2toKhjOuAg')
+  client.send_sticker(message.chat.id,'CAADBQADOgIAApvFRx_Vb9xZ2tDZeAI')
  elif message.text == "4":
-  client.send_sticker(message.chat.id,'CAADBQADOAADy-MGFcTomafSvj85Ag')
+  client.send_sticker(message.chat.id,'CAADBQADOQIAApvFRx-X3zKjUZBD-AI')
  elif "WICKET" in message.text:
-  client.send_sticker(message.chat.id,'CAADBQADQgADy-MGFQKaDFrV7dM4Ag')
+  client.send_sticker(message.chat.id,'CAADBQADOwIAApvFRx-9TLj8euFGYwI')
  elif 'DRINKS BREAK' in message.text:
-  client.send_sticker(message.chat.id,'CAADBQADRgADy-MGFUpRV_38qCywAg')
+  client.send_sticker(message.chat.id,'CAADBQADJQAD271NHRSHuFn7xmbvAg')
  elif 'WIDE' in message.text:
-  client.send_sticker(message.chat.id,'CAADBQADQQADy-MGFbOcRX3V6UQ6Ag')
+  client.send_sticker(message.chat.id,'CAADBQADQQIAApvFRx-ATGFrOeEkHwI')
  elif 'DEAD BALL' in message.text:
-  client.send_sticker(message.chat.id,'CAADBQADPQADy-MGFdWUWIDhSc-DAg')
+  client.send_sticker(message.chat.id,'CAADBQADPAIAApvFRx_d_0_aLMZ0CwI')
  elif 'NB' in message.text:
-  client.send_sticker(message.chat.id,'CAADBQADOQADy-MGFW3th7obf0AiAg')
+  client.send_sticker(message.chat.id,'CAADBQADPAIAApvFRx_d_0_aLMZ0CwI')
  elif 'CLOSING' in message.text:
-  client.send_sticker(message.chat.id,'CAADBQADRwADy-MGFb1ffrrgFbgbAg')
+  client.send_sticker(message.chat.id,'CAADBQADPgIAApvFRx9uJ-Thu3QeQQI')
  
 
 @app.on_message(Filters.private & Filters.sticker)
