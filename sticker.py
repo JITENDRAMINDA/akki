bd2a148913054e11c1e9d4bd415cac30a9042e85
JITENDRAMINDA <49564169+JITENDRAMINDA@users.noreply.github.com>
2019-09-14 07:13:11 +0530

    Update sticker.py

--- sticker.py
+++ sticker.py
@@ -1,30 +1,29 @@
 from pyrogram import Client, Filters
 import time
 from pyrogram.errors import FloodWait
-app = Client ("ssss",bot_token="930207168:AAHEUI6NMdETxUJe9hfYR1P4BN-n5VRMKHY",api_id=814511,api_hash="44462f0f278503255d5cc30941b617a9")                                   
-
+app = Client ("ssss",bot_token="959566620:AAGdt9-HIPcefOmfIrIZNSRc8vuqg_UVxgM",api_id=814511,api_hash="44462f0f278503255d5cc30941b617a9")                                   
 @app.on_message(Filters.channel)
 def main(client, message):
  if message.text == "6":
-  client.send_sticker(message.chat.id,'CAADBQADOgIAApvFRx_Vb9xZ2tDZeAI')
+  client.send_sticker(message.chat.id,'CAADBQADNwADy-MGFaBs2toKhjOuAg')
  elif message.text == "4":
-  client.send_sticker(message.chat.id,'CAADBQADOQIAApvFRx-X3zKjUZBD-AI')
+  client.send_sticker(message.chat.id,'CAADBQADOAADy-MGFcTomafSvj85Ag')
  elif "WICKET" in message.text:
-  client.send_sticker(message.chat.id,'CAADBQADOwIAApvFRx-9TLj8euFGYwI')
+  client.send_sticker(message.chat.id,'CAADBQADQgADy-MGFQKaDFrV7dM4Ag')
  elif 'DRINKS BREAK' in message.text:
-  client.send_sticker(message.chat.id,'CAADBQADJQAD271NHRSHuFn7xmbvAg')
+  client.send_sticker(message.chat.id,'CAADBQADRgADy-MGFUpRV_38qCywAg')
  elif 'WIDE' in message.text:
-  client.send_sticker(message.chat.id,'CAADBQADQQIAApvFRx-ATGFrOeEkHwI')
+  client.send_sticker(message.chat.id,'CAADBQADQQADy-MGFbOcRX3V6UQ6Ag')
  elif 'DEAD BALL' in message.text:
-  client.send_sticker(message.chat.id,'CAADBQADPAIAApvFRx_d_0_aLMZ0CwI')
- elif 'NB' in message.text:
-  client.send_sticker(message.chat.id,'CAADBQADPAIAApvFRx_d_0_aLMZ0CwI')
+  client.send_sticker(message.chat.id,'CAADBQADPQADy-MGFdWUWIDhSc-DAg')
+ elif 'NO BALL' in message.text:
+  client.send_sticker(message.chat.id,'CAADBQADOQADy-MGFW3th7obf0AiAg')
  elif 'CLOSING' in message.text:
-  client.send_sticker(message.chat.id,'CAADBQADPgIAApvFRx9uJ-Thu3QeQQI')
+  client.send_sticker(message.chat.id,'CAADBQADRwADy-MGFb1ffrrgFbgbAg')
  elif 'WD' in message.text:
-  client.send_sticker(message.chat.id,'CAADBQADQQIAApvFRx-ATGFrOeEkHwI')
+  client.send_sticker(message.chat.id,'CAADBQADQQADy-MGFbOcRX3V6UQ6Ag')
  elif "WKT" in message.text:
-  client.send_sticker(message.chat.id,'CAADBQADOwIAApvFRx-9TLj8euFGYwI')
+  client.send_sticker(message.chat.id,'CAADBQADQgADy-MGFQKaDFrV7dM4Ag')
 @app.on_message(Filters.private & Filters.sticker)
 def forawrd(client, message):
   client.send_message(message.chat.id,message.sticker.file_id )
