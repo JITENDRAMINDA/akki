7f47380324006eb8d90691f6522c51edf3104fac
JITENDRAMINDA <49564169+JITENDRAMINDA@users.noreply.github.com>
2019-09-17 17:23:02 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ from pyrogram.errors import FloodWait
 app = Client ("ssss",bot_token="867420152:AAEQWq_rljX_q_0WBJeY1yOqYUNmRXBsEz8",api_id=814511,api_hash="44462f0f278503255d5cc30941b617a9")                                   
 bullet = -1001289914295                                              
 ferrari = -1001453099412                                             
-k = -1001425217224
+k = -1001487011678
 @app.on_message(Filters.chat(bullet) & ~ Filters.edited)
 def main(client, message):
  mes = client.send_message( k, "**" + message.text.replace("🇩🇪","🇳🇮").replace("🎾","🥎").replace("🖲","🧤").replace("📟","🏮").replace("WD","🕵️ WIDE BALL 🕵️").replace("/","➠").replace("CHALU RAKHO","🤾 GAME STARTED 🚶, ⛹️ PLAYERS ON THE STEDIUM 🏌️").replace("NB","🙅 NO BALL 🙅") + "**" )
