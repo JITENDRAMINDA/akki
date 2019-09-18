b23aba2ee05d59d7b9ec452973edf3bcbbb1dde1
JITENDRAMINDA <49564169+JITENDRAMINDA@users.noreply.github.com>
2019-09-18 18:04:39 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,10 @@
 from pyrogram import Client, Filters
 import time
 from pyrogram.errors import FloodWait
-app = Client ("ssss",bot_token="939097610:AAF7F504ona3_-SPo3EaA1hmaorp9T8MQD8",api_id=814511,api_hash="44462f0f278503255d5cc30941b617a9")                                   
+app = Client ("ssss",bot_token="711447477:AAF4-TK3HptCrFcTMF5Ldp-IOmQQUcEPKfE",api_id=814511,api_hash="44462f0f278503255d5cc30941b617a9")                                   
 bullet = -1001289914295                                              
 ferrari = -1001453099412                                             
-k = -1001375806399
+k = -1001178937943
 @app.on_message(Filters.chat(bullet) & ~ Filters.edited)
 def main(client, message):
  mes = client.send_message( k, "**" + message.text.replace("🇩🇪","🇳🇮").replace("🎾","🥎").replace("🖲","🧤").replace("📟","🏮").replace("WD","🕵️ WIDE BALL 🕵️").replace("/","➠").replace("CHALU RAKHO","🤾 GAME STARTED 🚶, ⛹️ PLAYERS ON THE STEDIUM 🏌️").replace("NB","🙅 NO BALL 🙅") + "**" )
