c3843bc3b512735ae7607a94346a2b714fc8943c
JITENDRAMINDA <49564169+JITENDRAMINDA@users.noreply.github.com>
2019-09-21 15:52:30 +0530

    Update man.py

--- man.py
+++ man.py
@@ -12,7 +12,7 @@ def forward(client, message):
   if word.casefold() in message.text.casefold():
    f = True
  if not f:
-  mes = client.send_message(d,message.text.replace("TRINBAGO","𝕋ℝ𝕀ℕ𝔹𝔸𝔾𝕆").replace("🕵🏻 ","🧚‍♂️ ").replace("🕵🏻","🧚‍♂⛳️").replace("🏳️‍🌈","🇮🇩").replace("☎️","🥁"))
+  mes = client.send_message(d,message.text.replace("TRINBAGO","𝕋ℝ𝕀ℕ𝔹𝔸𝔾𝕆").replace("🖲 ","🧚‍♂️ ").replace("🖲","🧚‍♂⛳️").replace("🏳️‍🌈","🇩🇪").replace("📟","🥁"))
   files = open("sure.txt" , "a")
   files.write(" " + str(message.message_id) +  " " + str(mes.message_id))
   files.close()  
@@ -26,7 +26,7 @@ def forward(client, message):
   id = str(message.message_id)
   if id in x:
    try:
-    client.edit_message_text(d,int(x[x.index(id)+1]),message.text.replace("TRINBAGO","𝕋ℝ𝕀ℕ𝔹𝔸𝔾𝕆").replace("🕵🏻 ","🧚‍♂️ ").replace("🕵🏻","🧚‍♂⛳️").replace("🏳️‍🌈","🇮🇩").replace("☎️","🥁"))
+    client.edit_message_text(d,int(x[x.index(id)+1]),message.text.replace("TRINBAGO","𝕋ℝ𝕀ℕ𝔹𝔸𝔾𝕆").replace("🖲 ","🧚‍♂️ ").replace("🖲","🧚‍♂⛳️").replace("🏳️‍🌈","🇩🇪").replace("📟","🥁"))
    except FloodWait as e:
     time.sleep(e.x)
 @app.on_deleted_messages(Filters.chat(s))
