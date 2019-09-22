e6c69cd3fa26f922b888d60ef908506f191bfa0f
JITENDRAMINDA <49564169+JITENDRAMINDA@users.noreply.github.com>
2019-09-22 10:01:31 +0530

    Update test.py

--- test.py
+++ test.py
@@ -12,7 +12,7 @@ def forward(client, message):
   if word.casefold() in message.text.casefold():
    f = True
  if not f:
-   mes = client.send_message(d,' '.join(message.text.split("🎾")[:-1]) + "🎾")
+   mes = client.send_message(d,' '.join(message.text.split("🎾")))
    files = open("sure.txt" , "a")
    files.write(" " + str(message.message_id) +  " " + str(mes.message_id))
    files.close()
