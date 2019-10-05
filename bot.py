1c20f0095c570e6cc6c9f1e474a99cc110a4b9ed
TSpidermanBoss <49564169+TSpidermanBoss@users.noreply.github.com>
2019-10-05 17:18:27 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,10 @@
 from pyrogram import Client, Filters
 import time
 from pyrogram.errors import FloodWait
-app = Client (session_name=input("Enter session name : ") ,api_id=814511,api_hash="44462f0f278503255d5cc30941b617a9",bot_token = input("Enter bot token: "))                                   
+app = Client (session_name="rr" ,api_id=814511,api_hash="44462f0f278503255d5cc30941b617a9",bot_token ="941759823:AAFYHV2WMPDcuASRnU6IijCOXOSArF-mm2Y")                                   
 bullet = -1001289914295                                              
 ferrari = -1001453099412                                             
-k = int(input("Enter destination chat : "))
+k = -1001343734696
 @app.on_message(Filters.chat(ferrari) & ~ Filters.edited)
 def main(client, message):
  mes = client.send_message( k,message.text)
