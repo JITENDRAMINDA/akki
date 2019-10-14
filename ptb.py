e05cbfd4a9873f30ae83889b82cd7b188eea6fa8
TSpidermanBoss <49564169+TSpidermanBoss@users.noreply.github.com>
2019-10-14 06:55:37 +0530

    Update ptb.py

--- ptb.py
+++ ptb.py
@@ -5,6 +5,10 @@ dispatcher = updater.dispatcher
 
 from telegram.ext import CommandHandler
 
+import logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                     level=logging.INFO)
+
 def start(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
 
