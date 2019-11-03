f3216e01cf8726d3fb5a2014156f5b54a7b9eea6
TSpidermanBoss <49564169+TSpidermanBoss@users.noreply.github.com>
2019-11-03 10:17:51 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,8 +2,8 @@ from pyrogram import Client, Filters
 import time
 from pyrogram.errors import FloodWait
 
-k = -1001277117445
-bot = "1058039485:AAFMVVXkTYzO3PbsKXYL5qFlWT-ho8ipOvU"
+k = -1001320593844
+bot = "1042391911:AAEybqPrXTcxMcWVp1lCjMB4dDi88t8B6yQ"
 app = Client (session_name="rr",api_id=814511,api_hash="44462f0f278503255d5cc30941b617a9",bot_token = bot)                                   
 bullet = -1001289914295                                              
 ferrari = -1001453099412                                             
